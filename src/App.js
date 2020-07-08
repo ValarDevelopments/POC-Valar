0cca0bb2d5965daef664dbe312b66362a2944d89
Curro <currocuo9625@mail.com>
2020-07-08 19:31:45 +0200

    Start with the basic structure(Header, Footer, Main)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./Images/fairy-shield.png";
+import { Divider, Form, Label } from "semantic-ui-react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>Valar Development</div>
       </header>
+
+      <main className="App-main">
+        <div className="App-center">
+          <Form>
+            <Form.Field>
+              <input type="text" placeholder="First name" />
+              <Label pointing>Please enter a value</Label>
+            </Form.Field>
+            <Divider />
+
+            <Form.Field>
+              <Label pointing="below">Please enter a value</Label>
+              <input type="text" placeholder="Last Name" />
+            </Form.Field>
+            <Divider />
+
+            <Form.Field inline>
+              <input type="text" placeholder="Username" />
+              <Label pointing="left">That name is taken!</Label>
+            </Form.Field>
+            <Divider />
+
+            <Form.Field inline>
+              <Label pointing="right">
+                Your password must be 6 characters or more
+              </Label>
+              <input type="password" placeholder="Password" />
+            </Form.Field>
+          </Form>
+        </div>
+      </main>
+      <footer className="App-footer">
+        <div>Curro Casado - Miguel Rodin</div>
+      </footer>
     </div>
   );
 }
